06f7586160162fc9db561d8062a185449688a8ab
BraenAndDany <braenanddany@mail.ru>
2023-10-02 16:03:38 +0700

    Добавьте файлы проекта.

---
+++ tetris/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris
+{
+    public partial class Form1 : Form
+    {
+        Shape currentShape;
+        int size;
+        int[,] map = new int[8, 16];
+        Timer timer1 = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public void Init()
+        {
+            size = 25;
+
+            currentShape = new Shape(3, 0);
+
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(update);
+
+            Invalidate();
+        }
+        private void update(object sender, EventArgs e)
+        {
+            currentShape.MoveDown();
+            Invalidate();
+        }
+        public void Merge()
+        {
+            for (int i = currentShape.y; i < 3; i++)
+            {
+                for(int j = currentShape.x; j < currentShape.x+3; j++)
+                {
+                    map[i, j] = currentShape.matrix[i-currentShape.y,j-currentShape.x];
+                }
+            }
+        }
+        public void ResetArea()
+        {
+            for (int i = currentShape.y; i < 3; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + 3; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+        public void DrawMap(Graphics e)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                for (int j=0;j>16;j++)
+                {
+                    if (map[i,j]==1)
+                    {
+                        e.FillRectangle(Brushes.Red, new Rectangle(50+j*size,50+i*size,size,size));
+                    }
+                }
+            }
+        }
+        public void DrawGrid(Graphics g)
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                    g.DrawLine(Pens.Black, new Point(50,50 + i*size), new Point(50+8*size, 50+i*size));
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                g.DrawLine(Pens.Black, new Point(50 + i * size,50), new Point(50 + i * size,50 + 16 * size));
+            }
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(e.Graphics);
+        }
+    }
+}
